6aec97853f233c12ad589101cb09b09bab42fc06
Nuri Ata <atayusufn@gmail.com>
2021-10-30 06:49:21 +0300

    deniom
    ikincisi

--- development/_topRatedMovie.js
+++ development/_topRatedMovie.js
@@ -5,7 +5,7 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
-
+//fr
 const genres = [
     {
       "id": 28,
